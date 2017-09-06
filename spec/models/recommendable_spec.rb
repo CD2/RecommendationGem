6f6dd5018931b9385275d64eb8e54373449f3718
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2017-09-06 13:34:33 +0100

    some tests working

--- spec/models/recommendable_spec.rb
+++ spec/models/recommendable_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe 'recommendable' do
   subject(:user) { FactoryGirl.create :user }
 
-  let!(:a1) {
-    x = FactoryGirl.create :article
-    x.tag_with(:race_cars, :speed_holes, :go_faster_stripes)
-    x
-  }
-  let!(:a2) {
-    x = FactoryGirl.create :article
-    x.tag_with(:race_cars, :flame_decals, :speed_holes)
-    x
-  }
-  let!(:a3) {
-    x = FactoryGirl.create :article
-    x.tag_with()
-    x
-  }
-
-  before(:each) do
-    user.tag_with(race_cars: 10, flame_decals: 5)
-  end
+  # let!(:a1) {
+  #   x = FactoryGirl.create :article
+  #   x.tag_with(:race_cars, :speed_holes, :go_faster_stripes)
+  #   x
+  # }
+  # let!(:a2) {
+  #   x = FactoryGirl.create :article
+  #   x.tag_with(:race_cars, :flame_decals, :speed_holes)
+  #   x
+  # }
+  # let!(:a3) {
+  #   x = FactoryGirl.create :article
+  #   x.tag_with()
+  #   x
+  # }
+  #
+  # before(:each) do
+  #   user.tag_with(race_cars: 10, flame_decals: 5)
+  # end
 
   describe '#tag_with' do
 
@@ -31,28 +31,28 @@ RSpec.describe 'recommendable' do
 
       it 'assign tag1 without weight' do
         target.tag_with(:tag1)
-        expect(target.tags).to match( [{'tag1':1}] )
+        expect(target.r_document.static_tags['tag1']).to match(1)
       end
 
       it 'assign tag1 with weight' do
         target.tag_with(tag1: 10)
-        expect(target.tags).to match( [{'tag1':10}] )
+        expect(target.r_document.static_tags['tag1']).to match(10)
       end
 
-      it '.tag should return array of hashs' do
+      it '.tag should return hash' do
         target.tag_with(:tag1)
         target.tag_with(:tag2)
-        expect(target.tags).to match( [{'tag1':1}, {'tag2':1}] )
+        expect(target.r_document.static_tags).to match( {'tag1' => 1, 'tag2' => 1} )
       end
 
       it 'when the tag already exists, it is not overwritten' do
-        target.tag_with(:tag1: 10)
+        target.tag_with(tag1: 10)
         target.tag_with(:tag1)
-        expect(target.tags.first.weight).to eq(10)
+        expect(target.r_document.static_tags['tag1']).to eq(10)
       end
     end
 
-    describe 'assigning multiple tags at a time' do
+    pending 'assigning multiple tags at a time' do
       it 'should consider a hash of tags as tag_name/weight pairs' do
         target.tag_with( [{tag1:1}, {tag2:2}, {tag3:3}] )
         expect(target.tags).to match([{tag1:1}, {tag2:2}, {tag3:3}])
@@ -66,19 +66,19 @@ RSpec.describe 'recommendable' do
     end
   end
 
-  describe '#recommend_to' do
+  pending '#recommend_to' do
+    let(:target1) { FactoryGirl.create(:article) }
+    let(:target2) { FactoryGirl.create(:article) }
+    let(:target3) { FactoryGirl.create(:article) }
+    let(:user) { FactoryGirl.create(:user) }
     context 'when all article and user tags have weight 1' do
-        let(:target1) { FactoryGirl.create(:article) }
-        let(:target2) { FactoryGirl.create(:article) }
-        let(:target3) { FactoryGirl.create(:article) }
-        let(:user) { FactoryGirl.create(:user) }
       it 'orders the results by the number of common tags' do
         target1.tag_with(:tag1)
         target2.tag_with(:tag1)
         target2.tag_with(:tag2)
         user.tag_with(:tag1)
         user.tag_with(:tag2)
-        expect(Articles.recommend_to(user, :common)).to eq([target2, target1])
+        expect(Article.recommend_to(user)).to eq([target2, target1])
       end
     end
 
@@ -90,7 +90,7 @@ RSpec.describe 'recommendable' do
         user.tag_with(tag1:2)
         user.tag_with(tag2:5)
         user.tag_with(tag3:9)
-        expect(Articles.recommend_to(user, :relevance)).to eq([target3, target2, target1])
+        expect(Article.recommend_to(user)).to eq([target3, target2, target1])
       end
     end
 
@@ -102,7 +102,7 @@ RSpec.describe 'recommendable' do
         target1.tag_with(tag1:4)
         target2.tag_with(tag2:6)
         target3.tag_with(tag3:8)
-        expect(Articles.recommend_to(user, :relevance)).to eq([target3, target2, target1])
+        expect(Article.recommend_to(user)).to eq([target3, target2, target1])
       end
     end
 
@@ -115,7 +115,7 @@ RSpec.describe 'recommendable' do
         target1.tag_with(tag1: 8)
         target2.tag_with(tag2: -1)
         target3.tag_with(tag3: 7)
-        expect(Articles.recommend_to(user, :relevance)).to eq([target3, target1, target2])
+        expect(Article.recommend_to(user)).to eq([target3, target1, target2])
       end
       it 'should weight user tags more than article one\'s, I think?'
       it 'is better to see something somewhat relevant to a major interest that to see something very relevant to a minor interest, maybe?'
@@ -131,7 +131,7 @@ RSpec.describe 'recommendable' do
       target2.tag_with(tag1: 10)
       target2.tag_with(tag5: 10)
 
-      expect(Articles.recommend_to(user, :relevance)).to eq([target2, target1])
+      expect(Article.recommend_to(user)).to eq([target2, target1])
     end
     it 'should consider popularity?'
 
