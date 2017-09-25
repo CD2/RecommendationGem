ba97f4b29e72b2c35c17ecd6dd4a2e940cebc536
James Page <james.page@cd2solutions.co.uk>
2017-09-25 12:50:39 +0100

    merge tags

--- app/models/recommendation/document.rb
+++ app/models/recommendation/document.rb
@@ -15,15 +15,45 @@ module Recommendation
       pluck('DISTINCT jsonb_object_keys(tags_cache)')
     end
 
-    def self.tagged_with(tag_names, allow_negative = false)
+    def self.tagged_with(tag_names, allow_negative = false, any = false)
       tag_names.map! { |x| normalize(x) }
       query_chain do
         expand_json(:tags_cache)
         where('key IN (?)', tag_names)
         where('value::numeric > 0') unless allow_negative
         group(:id)
-        having('COUNT(key) = ?', tag_names.count)
+        if any
+          having('COUNT(key) > 0')
+        else
+          having('COUNT(key) = ?', tag_names.count)
+        end
       end
+    end
+
+    def self.merge_tags(tag_names, new_name)
+      tag_names.map! { |x| normalize(x) }
+      new_name = normalize(new_name)
+
+      cache_score = tag_names.map { |x|
+        "coalesce((tags_cache ->> '#{x}')::numeric, 0)"
+      }.join(' + ')
+      cache_entry = "jsonb_build_object('#{new_name}', #{cache_score})"
+
+      static_score = tag_names.map { |x|
+        "coalesce((static_tags ->> '#{x}')::numeric, 0)"
+      }.join(' + ')
+      static_entry = "jsonb_build_object('#{new_name}', #{static_score})"
+
+      remove_keys = tag_names.map { |x| "- '#{x}'" }.join(' ')
+
+      sql_execute %{
+        UPDATE recommendation_documents
+        SET
+          tags_cache = tags_cache #{remove_keys} || #{cache_entry},
+          static_tags = static_tags #{remove_keys} || #{static_entry}
+        WHERE
+          id IN (#{tagged_with(tag_names, true, true).select(:id).as_sql})
+      }
     end
 
     delegate :parse_special_tag, :compose_special_tag, :method_missing,
